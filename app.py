77ece4244d9746a0008847937bbdd7edb0a8d304
Lakshay Lk <142203048+lakshaykun@users.noreply.github.com>
2024-07-27 16:13:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import random
 
 client = MongoClient(
     "mongodb+srv://lakshu1000:lakshay1920@mlprojects.n13dkun.mongodb.net/&ssl=true&ssl_cert_reqs=CERT_NONE",
-    ssl=True,
-    ssl_cert_reqs='CERT_NONE',
     serverSelectionTimeoutMS=50000,  # Increase the timeout
     connectTimeoutMS=50000,
     socketTimeoutMS=50000
